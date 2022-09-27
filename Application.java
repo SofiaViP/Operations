fce412c8872f91f44f6299740b22fbf86ee33ee0
Du <luispedro.2003@hotmail.com>
2022-09-27 16:41:06 +0100

    Added multiple operations

--- Application.java
+++ Application.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
+        String operation = scanner.nextLine();
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        System.out.println(a + b);
+        if (operation.equals("sum")){
+            System.out.println(a + b);}
+        else if (operation.equals("mul")) {
+            System.out.println(a * b);
+        }
+        else if (operation.equals("div")){
+            System.out.println(a / b);
+        }
+        else {
+            System.out.println(a - b);}
     }
 }
